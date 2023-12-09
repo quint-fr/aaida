33dbec1a57a1d07543913952c9566c1bf56cbbf1
quint-abs <paul.michelot@gmail.com>
2023-12-09 14:19:27 +0100

    Skeletons are spooky
    Cons :
    - Uploaded a DB
    - Need testing
    - Tesseract requirement's still here
    Pros :
    - SFW
    - Added licence ?

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aaida
-Handler for shitty stuff to do on aaida
+# AAIDA
+Handler for shitty stuff not handled by AAIDA
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * I wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.  Anomie
+ * ----------------------------------------------------------------------------
+*/
--- src/aaida.py
+++ src/aaida.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python3
 
+ERROR_MSG = """Usage : $ aaida"""
+
 import requests as req
 
 import sys
+import csv
+import random as rnd
+import json
 import re
+import time
 import json
 
 #"""await fetch("https://aaida.restosducoeur.org/pickup/add", {
@@ -33,15 +39,6 @@ import json
 #  }
 #}
 
-CSRF = None
-POST_ADD = 'createdAt={}&provider={}&providerToAdd%5Bname%5D=&providerToAdd%5Baddress%5D=&providerToAdd%5Bcontact%5D=&declaredWeight={}&realWeight={}&dispatchableWeight={}&_token=65abba54db31.y81hJ_5qsEryEZnNpLLh2PwVgxuq2infRBLE7a7TheY.of0wQbgExh7GPLS148PYjM5X3Hrp6nOYLkCpgvaa6bP-_xVtv17AFaROoQ'
-createdAt = "%Y-%m-%d"
-provider = ""
-declared = ""
-def pp_json(d):
-  print(d)
-  print(json.dumps(d, indent=2))
-
 def get_csrf():
   url = "https://aaida.restosducoeur.org/"
   res = req.get(url)
@@ -49,18 +46,142 @@ def get_csrf():
                         value="(.*)"''', res.text)
   return r[0]
 
-def get_pickups(csrf, cookie, d):
-  headers = {"Cookie" : cookie}
-  url = "https://aaida.restosducoeur.org/pickup/add"
-  # get csrf_token here
-  #res = req.get(url, headers=headers)
-  d["_token"] = csrf
-  res = req.post(url, headers=headers, data=d)
+def assign_input_spec(d, t, c):
+  if d[t]:
+    return d[t]
+  tot = int(int(d["disp"]) * rnd.randint(*c) / 100)
+  d[t] = str(tot)
+  return tot
 
-  return res.json()["id"]
+def get_input(name):
+  """Retrieve donations value from csv"""
+  with open(name) as f:
+    res = []
+    for l in csv.DictReader(f, delimiter=";"):
+      if not l["real"]:
+        l["real"] = l["decl"]
+      if not l["disp"]:
+        l["disp"] = l["real"]
+      tot = int(l["real"])
+      for t in ("prot", "side", "diar", "dess"):
+        # empirical assign
+        tot -= assign_input_spec(l, t, (10, 30))
+      # we don't do misc unless specifics (go manual)
+      l["misc"] = 0
+      # side is at least 10%<>70%
+      l["side"] = int(float(l["side"])) + tot
+      res.append(l)
+  return res
+
+## Used to retrieve providers codes
+def get_providers(cookie):
+  url = "https://aaida.restosducoeur.org/providers"
+  r = req.get(url, headers={"Cookie": "PHPSESSID={}".format(cookie)})
+  s = r.text
+  ns = re.findall('''"Nom">([^<]*)</td>''', s)
+  ps = re.findall('''/provider/([0-9]+)/''', s)
+  res = [_ for _ in zip(ns, ps)]
+  print(res)
+  return ns, ps
+
+def load_providers():
+  with open("prov.db") as f:
+    return json.load(f)
+
+def add_pickup(cookie):
+  """GET : Let's get it started."""
+  url = "https://aaida.restosducoeur.org/pickup/add"
+  res = req.get(url, headers={"Cookie": "PHPSESSID={}".format(cookie)})
+  csrf = re.findall("name=\\\\u0022_token\\\\u0022 value=\\\\u0022(.*)\\\\u0022", res.text)[0]
+  return csrf
+
+def post_pickup(cookie, csrf, d):
+  """POST : Don't mess up the input."""
+  data = '\
+createdAt={}&\
+provider={}\
+providerToAdd%5Bname%5D=&providerToAdd%5Baddress%5D=&providerToAdd%5Bcontact%5D=&declaredWeight={}\
+&realWeight={}\
+&dispatchableWeight={}\
+&_token={}'.format(
+    d["date"],
+    d["code"],
+    d["decl"],
+    d["real"],
+    d["disp"],
+    csrf
+  )
+  print("Supposedly posted pickup :", data, "with id", "FUCK OFF")
+  #r = req.post(url, headers={"Cookie": "PHPSESSID={}".format(cookie)}, data=data)
+  r = {"success" : "test", "id": "ImJustSnacking"}
+  return r
+
+def add_distrib(cookie, csrf, d):
+  """GET : Under the radar."""
+  url = "https://aaida.restosducoeur.org/pickup/distribute/{}".format(d["rid"])
+  #res = req.get(url, headers={"Cookie": "PHPSESSID={}".format(cookie)})
+  #token = re.findall("""name="distribution[_token]" value="([^"]*)""", res.text)[0]
+  return token
+
+def post_distrib(cookie, csrf, d):
+  """POST : I hope it works."""
+  url = "https://aaida.restosducoeur.org/pickup/distribute/{}".format(d["rid"])
+  data = "\
+distribution%5Baxis%5D%5B0%5D%5Bweight%5D={}\
+&distribution%5Baxis%5D%5B1%5D%5Bweight%5D={}\
+&distribution%5Baxis%5D%5B2%5D%5Bweight%5D={}\
+&distribution%5Baxis%5D%5B3%5D%5Bweight%5D={}\
+&distribution%5Baxis%5D%5B4%5D%5Bweight%5D={}\
+&distribution%5B_token%5D={}".format(
+  d["prot"],
+  d["side"],
+  d["diar"],
+  d["dess"],
+  d["misc"],
+  csrf
+)
+  #res = req.post(url, headers={"Cookie": "PHPSESSID={}".format(cookie)}, data)
+  r = {"result" : "test"}
+  return res.text
 
 if __name__ == "__main__":
-  cookie, csrf = sys.argv[1], sys.argv[2]
-  res = get_pickups()
-  pp_json(res)
+  name = sys.argv[1]
+  cookie = sys.argv[2]
 
+  # Read inputs
+  d = get_input(name)
+  providers = load_providers()
+  #print(providers)
+
+  # Check inputs
+  # get_providers(cookie)
+  codes = set(providers.values())
+  for l in d:
+    if l["code"] in providers:
+      l["code"] = providers[l["code"]]
+    if l["code"] not in codes:
+      print("Pebcaked :", l)
+      exit(57)
+
+  # Sleep between each requests to avoid DDoS
+  sleeping_time = 3
+  print("Retrieved", len(d), "collect(s).")
+  for l in d:
+    print("Adding", l)
+    # Request new row
+    csrf = add_pickup(cookie)
+    print("csrf :", csrf)
+    time.sleep(sleeping_time)
+    rid = post_pickup(cookie, csrf, l)
+    l["rid"] = rid
+    print("rid :", rid)
+    time.sleep(sleeping_time)
+    token = add_distrib(cookie, csrf, l)
+    print("token", token)
+    time.sleep(sleeping_time)
+    res = post_distrib(cookie, csrf, l)
+    print("Uplodaded :", res.text)
+    time.sleep(sleeping_time)
+
+  ##res = get_pickups(csrf, cookie)
+  ##print(res.json())
